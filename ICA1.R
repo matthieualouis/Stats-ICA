5abfba4065c59d52c73abb5ab2066ecf458b3471
Matthieu Louis <Matthieu@Matthieus-MacBook-Pro.local>
2013-11-18 17:54:58 +0000

    Merged with remote

--- ICA1.R
+++ ICA1.R
@@ -25,10 +25,18 @@ nox.fit <- lm(Y ~ X[,1] + X[,2] + X[,3]);
 summary(nox.fit);
 nox.res <-residuals(nox.fit);
 nox.stdres <- rstandard(nox.fit);
-qqnorm(nox.stdres);
-qqline(nox.stdres, col = 2);
 
 # Some analytics plots
 par(mfrow=c(2,2))
 plot(nox.fit,which=c(1,2,3,4),ask=FALSE)
+
+# Plotting QQ plot of standardised Residuals
+qqnorm(nox.stdres);
+qqline(nox.stdres, col = 2);
+
+# Plotting Residuals with respect to Predictors:
+plot(nox.stdres,X[,1])
+plot(nox.stdres,X[,2])
+plot(nox.stdres,X[,3])
+
 
